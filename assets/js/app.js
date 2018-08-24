4782e363f237e4ea09ab62261483313641a7f7f7
Kuba Kułaga <kuba.kulaga.sv7@gmail.com>
2018-08-24 15:24:43 +0200

    configured project

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+import '../scss/app.scss'
+import $ from 'jquery'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/Main'
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
